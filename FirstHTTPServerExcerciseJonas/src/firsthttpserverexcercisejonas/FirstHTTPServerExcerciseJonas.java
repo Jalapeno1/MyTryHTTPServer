a136b8f01ad2662d0956645d59e148a10d6d5602
Jalapeno1 <jons_91@hotmail.com>
2014-08-29 10:24:42 +0200

    Added page that shows Request Headers

--- FirstHTTPServerExcerciseJonas/src/firsthttpserverexcercisejonas/FirstHTTPServerExcerciseJonas.java
+++ FirstHTTPServerExcerciseJonas/src/firsthttpserverexcercisejonas/FirstHTTPServerExcerciseJonas.java
@@ -32,18 +32,88 @@ public class FirstHTTPServerExcerciseJonas
         }
         HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
         server.createContext("/welcome", new RequestHandler());
+        server.createContext("/headers", new RequestHandler1());
         server.setExecutor(null); // Use the default executor
         server.start();
         System.out.println("Server started, listening on port: " + port);
     }
 
+    static class RequestHandler1 implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException
+        {
+            String response = "Welcome to my very second almost home made Web Server :-)";
+            
+            StringBuilder sb = new StringBuilder();
+            sb.append("<!DOCTYPE html>\n");
+            sb.append("<html>\n");
+            sb.append("<head>\n");
+            sb.append("<title>My fancy Web Site</title>\n");
+            sb.append("<meta charset='UTF-8'>\n");
+            sb.append("</head>\n");
+            sb.append("<body>\n");
+            
+            //table
+            sb.append("<table border=\"1\">");
+            sb.append("<tr>");
+            sb.append("<th>Header</th>");
+            sb.append("<th>Value</th>");
+            sb.append("</tr>");
+            sb.append("<tr>");
+            sb.append("<td>Cache-control</td>");
+            sb.append("<td>[max-age=0]</td>");
+            sb.append("</tr>");
+            sb.append("<tr>");
+            sb.append("<td>Host</td>");
+            sb.append("<td>[localhost:8080]</td>");
+            sb.append("</tr>");
+            sb.append("<tr>");
+            sb.append("<td>Accept-encoding</td>");
+            sb.append("<td>[gzip,deflate,sdch]</td>");
+            sb.append("</tr>");
+            sb.append("<tr>");
+            sb.append("<td>Connection</td>");
+            sb.append("<td>[keep-alive]</td>");
+            sb.append("</tr>");
+            sb.append("<tr>");
+            sb.append("<td>Accept-language</td>");
+            sb.append("<td>[da,en-US;q=0.8,en;q=0.6,en-GB;q=0.4]</td>");
+            sb.append("</tr>");
+            sb.append("<tr>");
+            sb.append("<td>User-agent</td>");
+            sb.append("<td>[Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like \nGecko) Chrome/36.0.1985.143 Safari/537.36]</td>");
+            sb.append("</tr>");
+            sb.append("<tr>");
+            sb.append("<td>Accept</td>");
+            sb.append("<td>[text/html,capplication/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8]</td>");
+            sb.append("</tr>");
+            
+            sb.append("</body>\n");
+            sb.append("</html>\n");
+            
+            response = sb.toString();
+            
+            
+            
+            Headers h = he.getResponseHeaders();
+            h.add("Content-Type", "text/html");
+            he.sendResponseHeaders(200, response.length());
+            try (PrintWriter pw = new PrintWriter(he.getResponseBody()))
+            {
+                pw.print(response); //What happens if we use a println instead of print --> Explain
+            }
+        }
+        
+    }
     static class RequestHandler implements HttpHandler
     {
         @Override
         public void handle(HttpExchange he) throws IOException
         {
             String response = "Welcome to my very first almost home made Web Server :-)";
-
+           
+// Opgave 1 -------------------------------------------------------------
             StringBuilder sb = new StringBuilder();
             sb.append("<!DOCTYPE html>\n");
             sb.append("<html>\n");
@@ -55,12 +125,13 @@ public class FirstHTTPServerExcerciseJonas
             sb.append("<h2>Welcome to my very first home made Web Server :-)</h2>\n");
             sb.append("</body>\n");
             sb.append("</html>\n");
+            
             response = sb.toString();
-
+            
+            
+            
             Headers h = he.getResponseHeaders();
             h.add("Content-Type", "text/html");
-                    
-            
             he.sendResponseHeaders(200, response.length());
             try (PrintWriter pw = new PrintWriter(he.getResponseBody()))
             {
